754f886511d0326e8dfb3bcc2f9543cc97b1580e
Pwn <hi@pwn.buzz>
2016-05-17 19:19:18 +0800

    setup grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+/* eslint-env node */
+
+module.exports = function ( grunt ) {
+
+  require( 'load-grunt-tasks' )( grunt )
+
+  grunt.initConfig( {
+    watch : {
+      is : {
+        files : 'lib/is.js' ,
+        tasks : 'uglify:is'
+      }
+    } ,
+    uglify : {
+      is : {
+        options : {
+          report : 'gzip' ,
+          sourceMap : true ,
+          sourceMapName : 'dist/is.min.map'
+        } ,
+        files : {
+          'dist/is.min.js' : 'lib/is.js'
+        }
+      }
+    }
+  } )
+
+  grunt.registerTask( 'default' , 'uglify:is' )
+
+}
