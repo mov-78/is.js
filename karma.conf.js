45d5a96619a36ac4c668b4ab7e4a9202f1358c0e
Pwn <hi@pwn.buzz>
2016-05-21 03:55:52 +0800

    updated karma config
    Signed-off-by: Pwn <hi@pwn.buzz>

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,6 @@
 module.exports = function ( config ) {
   config.set( {
-    browsers : [ 'Chrome' ] ,
-    files : [ 'lib/**/*.js' , 'test/**/*.js' ] ,
     frameworks : [ 'chai' , 'mocha' ] ,
-    reporters : [ 'mocha' ]
+    files : [ 'lib/**/*.js' , 'test/**/*.js' ]
   } )
 }
