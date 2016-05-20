8de74751b188c5f0a68e17ca239f72146b01dc0d
Pwn <hi@pwn.buzz>
2016-05-20 11:02:20 +0800

    optimize bundle:array

--- test/array.js
+++ test/array.js
@@ -1,80 +1,238 @@
 /* global expect, is */
 /* eslint-env mocha */
-/* eslint no-empty-function: 0, no-unused-expressions: 0 */
+/* eslint no-empty-function: 0, no-new-wrappers: 0, no-unused-expressions: 0 */
 
 describe( 'bundle:array' , function () {
 
-  var arrayLikeObject = { length : 0 }
-  var argumentsObject = ( function () { return arguments } )()
-
   it( 'is.array' , function () {
 
+    expect( is.array( null ) ).to.be.false
+    expect( is.array( void 0 ) ).to.be.false
+    expect( is.array( 0 ) ).to.be.false
+    expect( is.array( 0 / 0 ) ).to.be.false
+    expect( is.array( +1 / 0 ) ).to.be.false
+    expect( is.array( -1 / 0 ) ).to.be.false
+    expect( is.array( new Number( 0 ) ) ).to.be.false
+    expect( is.array( new Number( 0 / 0 ) ) ).to.be.false
+    expect( is.array( new Number( +1 / 0 ) ) ).to.be.false
+    expect( is.array( new Number( -1 / 0 ) ) ).to.be.false
+    expect( is.array( '' ) ).to.be.false
+    expect( is.array( new String( '' ) ) ).to.be.false
+    expect( is.array( true ) ).to.be.false
+    expect( is.array( false ) ).to.be.false
+    expect( is.array( new Boolean( true ) ) ).to.be.false
+    expect( is.array( new Boolean( false ) ) ).to.be.false
+    expect( is.array( {} ) ).to.be.false
+    expect( is.array( { length : 0 } ) ).to.be.false
+    expect( is.array( { length : '-' } ) ).to.be.false
     expect( is.array( [] ) ).to.be.true
-    expect( is.array( arrayLikeObject ) ).to.be.false
-    expect( is.array( argumentsObject ) ).to.be.false
+    expect( is.array( arguments ) ).to.be.false
+    expect( is.array( function () {} ) ).to.be.false
+    expect( is.array( new Date() ) ).to.be.false
+    expect( is.array( new Error() ) ).to.be.false
+    expect( is.array( /^/ ) ).to.be.false
 
+    expect( is.not.array( null ) ).to.be.true
+    expect( is.not.array( void 0 ) ).to.be.true
+    expect( is.not.array( 0 ) ).to.be.true
+    expect( is.not.array( 0 / 0 ) ).to.be.true
+    expect( is.not.array( +1 / 0 ) ).to.be.true
+    expect( is.not.array( -1 / 0 ) ).to.be.true
+    expect( is.not.array( new Number( 0 ) ) ).to.be.true
+    expect( is.not.array( new Number( 0 / 0 ) ) ).to.be.true
+    expect( is.not.array( new Number( +1 / 0 ) ) ).to.be.true
+    expect( is.not.array( new Number( -1 / 0 ) ) ).to.be.true
+    expect( is.not.array( '' ) ).to.be.true
+    expect( is.not.array( new String( '' ) ) ).to.be.true
+    expect( is.not.array( true ) ).to.be.true
+    expect( is.not.array( false ) ).to.be.true
+    expect( is.not.array( new Boolean( true ) ) ).to.be.true
+    expect( is.not.array( new Boolean( false ) ) ).to.be.true
+    expect( is.not.array( {} ) ).to.be.true
+    expect( is.not.array( { length : 0 } ) ).to.be.true
+    expect( is.not.array( { length : '-' } ) ).to.be.true
     expect( is.not.array( [] ) ).to.be.false
-    expect( is.not.array( arrayLikeObject ) ).to.be.true
-    expect( is.not.array( argumentsObject ) ).to.be.true
+    expect( is.not.array( arguments ) ).to.be.true
+    expect( is.not.array( function () {} ) ).to.be.true
+    expect( is.not.array( new Date() ) ).to.be.true
+    expect( is.not.array( new Error() ) ).to.be.true
+    expect( is.not.array( /^/ ) ).to.be.true
 
   } )
 
   it( 'is.arrayLike' , function () {
 
+    expect( is.arrayLike( null ) ).to.be.false
+    expect( is.arrayLike( void 0 ) ).to.be.false
+    expect( is.arrayLike( 0 ) ).to.be.false
+    expect( is.arrayLike( 0 / 0 ) ).to.be.false
+    expect( is.arrayLike( +1 / 0 ) ).to.be.false
+    expect( is.arrayLike( -1 / 0 ) ).to.be.false
+    expect( is.arrayLike( new Number( 0 ) ) ).to.be.false
+    expect( is.arrayLike( new Number( 0 / 0 ) ) ).to.be.false
+    expect( is.arrayLike( new Number( +1 / 0 ) ) ).to.be.false
+    expect( is.arrayLike( new Number( -1 / 0 ) ) ).to.be.false
     expect( is.arrayLike( '' ) ).to.be.true
+    expect( is.arrayLike( new String( '' ) ) ).to.be.true
+    expect( is.arrayLike( true ) ).to.be.false
+    expect( is.arrayLike( false ) ).to.be.false
+    expect( is.arrayLike( new Boolean( true ) ) ).to.be.false
+    expect( is.arrayLike( new Boolean( false ) ) ).to.be.false
+    expect( is.arrayLike( {} ) ).to.be.false
+    expect( is.arrayLike( { length : 0 } ) ).to.be.true
+    expect( is.arrayLike( { length : '-' } ) ).to.be.false
     expect( is.arrayLike( [] ) ).to.be.true
-    expect( is.arrayLike( arrayLikeObject ) ).to.be.true
-    expect( is.arrayLike( argumentsObject ) ).to.be.true
+    expect( is.arrayLike( arguments ) ).to.be.true
     expect( is.arrayLike( function () {} ) ).to.be.false
+    expect( is.arrayLike( new Date() ) ).to.be.false
+    expect( is.arrayLike( new Error() ) ).to.be.false
+    expect( is.arrayLike( /^/ ) ).to.be.false
 
+    expect( is.not.arrayLike( null ) ).to.be.true
+    expect( is.not.arrayLike( void 0 ) ).to.be.true
+    expect( is.not.arrayLike( 0 ) ).to.be.true
+    expect( is.not.arrayLike( 0 / 0 ) ).to.be.true
+    expect( is.not.arrayLike( +1 / 0 ) ).to.be.true
+    expect( is.not.arrayLike( -1 / 0 ) ).to.be.true
+    expect( is.not.arrayLike( new Number( 0 ) ) ).to.be.true
+    expect( is.not.arrayLike( new Number( 0 / 0 ) ) ).to.be.true
+    expect( is.not.arrayLike( new Number( +1 / 0 ) ) ).to.be.true
+    expect( is.not.arrayLike( new Number( -1 / 0 ) ) ).to.be.true
     expect( is.not.arrayLike( '' ) ).to.be.false
+    expect( is.not.arrayLike( new String( '' ) ) ).to.be.false
+    expect( is.not.arrayLike( true ) ).to.be.true
+    expect( is.not.arrayLike( false ) ).to.be.true
+    expect( is.not.arrayLike( new Boolean( true ) ) ).to.be.true
+    expect( is.not.arrayLike( new Boolean( false ) ) ).to.be.true
+    expect( is.not.arrayLike( {} ) ).to.be.true
+    expect( is.not.arrayLike( { length : 0 } ) ).to.be.false
+    expect( is.not.arrayLike( { length : '-' } ) ).to.be.true
     expect( is.not.arrayLike( [] ) ).to.be.false
-    expect( is.not.arrayLike( arrayLikeObject ) ).to.be.false
-    expect( is.not.arrayLike( argumentsObject ) ).to.be.false
+    expect( is.not.arrayLike( arguments ) ).to.be.false
     expect( is.not.arrayLike( function () {} ) ).to.be.true
-
-  } )
-  it( 'is.arrayLikeObject' , function () {
-
-    expect( is.arrayLikeObject( '' ) ).to.be.false
-    expect( is.arrayLikeObject( [] ) ).to.be.true
-    expect( is.arrayLikeObject( arrayLikeObject ) ).to.be.true
-    expect( is.arrayLikeObject( argumentsObject ) ).to.be.true
-    expect( is.arrayLikeObject( function () {} ) ).to.be.false
-
-    expect( is.not.arrayLikeObject( '' ) ).to.be.true
-    expect( is.not.arrayLikeObject( [] ) ).to.be.false
-    expect( is.not.arrayLikeObject( arrayLikeObject ) ).to.be.false
-    expect( is.not.arrayLikeObject( argumentsObject ) ).to.be.false
-    expect( is.not.arrayLikeObject( function () {} ) ).to.be.true
+    expect( is.not.arrayLike( new Date() ) ).to.be.true
+    expect( is.not.arrayLike( new Error() ) ).to.be.true
+    expect( is.not.arrayLike( /^/ ) ).to.be.true
 
   } )
 
   it( 'is.arguments' , function () {
 
+    expect( is.arguments( null ) ).to.be.false
+    expect( is.arguments( void 0 ) ).to.be.false
+    expect( is.arguments( 0 ) ).to.be.false
+    expect( is.arguments( 0 / 0 ) ).to.be.false
+    expect( is.arguments( +1 / 0 ) ).to.be.false
+    expect( is.arguments( -1 / 0 ) ).to.be.false
+    expect( is.arguments( new Number( 0 ) ) ).to.be.false
+    expect( is.arguments( new Number( 0 / 0 ) ) ).to.be.false
+    expect( is.arguments( new Number( +1 / 0 ) ) ).to.be.false
+    expect( is.arguments( new Number( -1 / 0 ) ) ).to.be.false
+    expect( is.arguments( '' ) ).to.be.false
+    expect( is.arguments( new String( '' ) ) ).to.be.false
+    expect( is.arguments( true ) ).to.be.false
+    expect( is.arguments( false ) ).to.be.false
+    expect( is.arguments( new Boolean( true ) ) ).to.be.false
+    expect( is.arguments( new Boolean( false ) ) ).to.be.false
+    expect( is.arguments( {} ) ).to.be.false
+    expect( is.arguments( { length : 0 } ) ).to.be.false
+    expect( is.arguments( { length : '-' } ) ).to.be.false
     expect( is.arguments( [] ) ).to.be.false
-    expect( is.arguments( arrayLikeObject ) ).to.be.false
-    expect( is.arguments( argumentsObject ) ).to.be.true
+    expect( is.arguments( arguments ) ).to.be.true
+    expect( is.arguments( function () {} ) ).to.be.false
+    expect( is.arguments( new Date() ) ).to.be.false
+    expect( is.arguments( new Error() ) ).to.be.false
+    expect( is.arguments( /^/ ) ).to.be.false
 
+    expect( is.not.arguments( null ) ).to.be.true
+    expect( is.not.arguments( void 0 ) ).to.be.true
+    expect( is.not.arguments( 0 ) ).to.be.true
+    expect( is.not.arguments( 0 / 0 ) ).to.be.true
+    expect( is.not.arguments( +1 / 0 ) ).to.be.true
+    expect( is.not.arguments( -1 / 0 ) ).to.be.true
+    expect( is.not.arguments( new Number( 0 ) ) ).to.be.true
+    expect( is.not.arguments( new Number( 0 / 0 ) ) ).to.be.true
+    expect( is.not.arguments( new Number( +1 / 0 ) ) ).to.be.true
+    expect( is.not.arguments( new Number( -1 / 0 ) ) ).to.be.true
+    expect( is.not.arguments( '' ) ).to.be.true
+    expect( is.not.arguments( new String( '' ) ) ).to.be.true
+    expect( is.not.arguments( true ) ).to.be.true
+    expect( is.not.arguments( false ) ).to.be.true
+    expect( is.not.arguments( new Boolean( true ) ) ).to.be.true
+    expect( is.not.arguments( new Boolean( false ) ) ).to.be.true
+    expect( is.not.arguments( {} ) ).to.be.true
+    expect( is.not.arguments( { length : 0 } ) ).to.be.true
+    expect( is.not.arguments( { length : '-' } ) ).to.be.true
     expect( is.not.arguments( [] ) ).to.be.true
-    expect( is.not.arguments( arrayLikeObject ) ).to.be.true
-    expect( is.not.arguments( argumentsObject ) ).to.be.false
+    expect( is.not.arguments( arguments ) ).to.be.false
+    expect( is.not.arguments( function () {} ) ).to.be.true
+    expect( is.not.arguments( new Date() ) ).to.be.true
+    expect( is.not.arguments( new Error() ) ).to.be.true
+    expect( is.not.arguments( /^/ ) ).to.be.true
 
   } )
 
   it( 'is.emptyArray' , function () {
 
+    expect( is.emptyArray( null ) ).to.be.false
+    expect( is.emptyArray( void 0 ) ).to.be.false
+    expect( is.emptyArray( 0 ) ).to.be.false
+    expect( is.emptyArray( 0 / 0 ) ).to.be.false
+    expect( is.emptyArray( +1 / 0 ) ).to.be.false
+    expect( is.emptyArray( -1 / 0 ) ).to.be.false
+    expect( is.emptyArray( new Number( 0 ) ) ).to.be.false
+    expect( is.emptyArray( new Number( 0 / 0 ) ) ).to.be.false
+    expect( is.emptyArray( new Number( +1 / 0 ) ) ).to.be.false
+    expect( is.emptyArray( new Number( -1 / 0 ) ) ).to.be.false
+    expect( is.emptyArray( '' ) ).to.be.false
+    expect( is.emptyArray( 'foo' ) ).to.be.false
+    expect( is.emptyArray( new String( '' ) ) ).to.be.false
+    expect( is.emptyArray( true ) ).to.be.false
+    expect( is.emptyArray( false ) ).to.be.false
+    expect( is.emptyArray( new Boolean( true ) ) ).to.be.false
+    expect( is.emptyArray( new Boolean( false ) ) ).to.be.false
+    expect( is.emptyArray( {} ) ).to.be.false
+    expect( is.emptyArray( { length : 0 } ) ).to.be.false
+    expect( is.emptyArray( { length : 5 } ) ).to.be.false
+    expect( is.emptyArray( { length : '-' } ) ).to.be.false
     expect( is.emptyArray( [] ) ).to.be.true
     expect( is.emptyArray( [ 0 ] ) ).to.be.false
     expect( is.emptyArray( new Array( 5 ) ) ).to.be.false
-    expect( is.emptyArray( arrayLikeObject ) ).to.be.false
-    expect( is.emptyArray( argumentsObject ) ).to.be.false
+    expect( is.emptyArray( arguments ) ).to.be.false
+    expect( is.emptyArray( function () {} ) ).to.be.false
+    expect( is.emptyArray( new Date() ) ).to.be.false
+    expect( is.emptyArray( new Error() ) ).to.be.false
+    expect( is.emptyArray( /^/ ) ).to.be.false
 
+    expect( is.not.emptyArray( null ) ).to.be.true
+    expect( is.not.emptyArray( void 0 ) ).to.be.true
+    expect( is.not.emptyArray( 0 ) ).to.be.true
+    expect( is.not.emptyArray( 0 / 0 ) ).to.be.true
+    expect( is.not.emptyArray( +1 / 0 ) ).to.be.true
+    expect( is.not.emptyArray( -1 / 0 ) ).to.be.true
+    expect( is.not.emptyArray( new Number( 0 ) ) ).to.be.true
+    expect( is.not.emptyArray( new Number( 0 / 0 ) ) ).to.be.true
+    expect( is.not.emptyArray( new Number( +1 / 0 ) ) ).to.be.true
+    expect( is.not.emptyArray( new Number( -1 / 0 ) ) ).to.be.true
+    expect( is.not.emptyArray( '' ) ).to.be.true
+    expect( is.not.emptyArray( 'foo' ) ).to.be.true
+    expect( is.not.emptyArray( new String( '' ) ) ).to.be.true
+    expect( is.not.emptyArray( true ) ).to.be.true
+    expect( is.not.emptyArray( false ) ).to.be.true
+    expect( is.not.emptyArray( new Boolean( true ) ) ).to.be.true
+    expect( is.not.emptyArray( new Boolean( false ) ) ).to.be.true
+    expect( is.not.emptyArray( {} ) ).to.be.true
+    expect( is.not.emptyArray( { length : 0 } ) ).to.be.true
+    expect( is.not.emptyArray( { length : 5 } ) ).to.be.true
+    expect( is.not.emptyArray( { length : '-' } ) ).to.be.true
     expect( is.not.emptyArray( [] ) ).to.be.false
     expect( is.not.emptyArray( [ 0 ] ) ).to.be.true
     expect( is.not.emptyArray( new Array( 5 ) ) ).to.be.true
-    expect( is.not.emptyArray( arrayLikeObject ) ).to.be.true
-    expect( is.not.emptyArray( argumentsObject ) ).to.be.true
+    expect( is.not.emptyArray( arguments ) ).to.be.true
+    expect( is.not.emptyArray( function () {} ) ).to.be.true
+    expect( is.not.emptyArray( new Date() ) ).to.be.true
+    expect( is.not.emptyArray( new Error() ) ).to.be.true
+    expect( is.not.emptyArray( /^/ ) ).to.be.true
 
   } )
 
