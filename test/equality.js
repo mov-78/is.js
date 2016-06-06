1b6fbed7dc09c955e719ccc6ed8924d4be4f4b8c
Pwn <hi@pwn.buzz>
2016-06-06 18:27:45 +0800

    remove inverse tests

--- test/equality.js
+++ test/equality.js
@@ -7,25 +7,15 @@ describe( 'bundle:equality' , function () {
   }
 
   it( 'is.equal' , function () {
-
     expect( is.equal( null , void 0 ) ).to.not.be.ok()
     expect( is.equal( 0 , 0 ) ).to.be.ok()
     expect( is.equal( 0 , '0' ) ).to.not.be.ok()
     expect( is.equal( +0 , -0 ) ).to.be.ok() // SameValueZero
     expect( is.equal( 0 / 0 , 0 / 0 ) ).to.be.ok() // SameValueZero
     expect( is.equal( [] , [] ) ).to.not.be.ok()
-
-    expect( is.not.equal( null , void 0 ) ).to.be.ok()
-    expect( is.not.equal( 0 , 0 ) ).to.not.be.ok()
-    expect( is.not.equal( 0 , '0' ) ).to.be.ok()
-    expect( is.not.equal( +0 , -0 ) ).to.not.be.ok() // SameValueZero
-    expect( is.not.equal( 0 / 0 , 0 / 0 ) ).to.not.be.ok() // SameValueZero
-    expect( is.not.equal( [] , [] ) ).to.be.ok()
-
   } )
 
   it( 'is.deepEqual' , function () {
-
     expect( is.deepEqual( null , void 0 ) ).to.not.be.ok()
     expect( is.deepEqual( 0 , 0 ) ).to.be.ok()
     expect( is.deepEqual( 0 , '0' ) ).to.not.be.ok()
@@ -38,20 +28,6 @@ describe( 'bundle:equality' , function () {
     expect( is.deepEqual( [ 1 , { foo : [ 2 , [ 3 , 4 ] ] , bar : { baz : 5 } } ] , [ 1 , { foo : [ 2 , [ 3 , 4 ] ] , bar : { baz : 5 } } ] ) ).to.be.ok()
     expect( is.deepEqual( [ 1 , { foo : [ 2 , [ 3 , 4 ] ] , bar : { baz : 5 } } ] , [ 1 , { foo : [ 2 , [ 3 , 4 ] ] , bar : { baz : 6 } } ] ) ).to.not.be.ok()
     expect( is.deepEqual( createObject( { foo : 1 } ) , createObject( { foo : 2 } ) ) ).to.be.ok()
-
-    expect( is.not.deepEqual( null , void 0 ) ).to.be.ok()
-    expect( is.not.deepEqual( 0 , 0 ) ).to.not.be.ok()
-    expect( is.not.deepEqual( 0 , '0' ) ).to.be.ok()
-    expect( is.not.deepEqual( +0 , -0 ) ).to.not.be.ok() // SameValueZero
-    expect( is.not.deepEqual( 0 / 0 , 0 / 0 ) ).to.not.be.ok() // SameValueZero
-    expect( is.not.deepEqual( [ 0 , [ 1 , [ 2 ] ] ] , [ 0 , [ 1 , [ 2 ] ] ] ) ).to.not.be.ok()
-    expect( is.not.deepEqual( [ 0 , [ 1 , [ 2 ] ] ] , [ 0 , [ 1 , [ 3 ] ] ] ) ).to.be.ok()
-    expect( is.not.deepEqual( { foo : { bar : 'baz ' } } , { foo : { bar : 'baz ' } } ) ).to.not.be.ok()
-    expect( is.not.deepEqual( { foo : { bar : 'baz ' } } , { foo : { bar : 'qux ' } } ) ).to.be.ok()
-    expect( is.not.deepEqual( [ 1 , { foo : [ 2 , [ 3 , 4 ] ] , bar : { baz : 5 } } ] , [ 1 , { foo : [ 2 , [ 3 , 4 ] ] , bar : { baz : 5 } } ] ) ).to.not.be.ok()
-    expect( is.not.deepEqual( [ 1 , { foo : [ 2 , [ 3 , 4 ] ] , bar : { baz : 5 } } ] , [ 1 , { foo : [ 2 , [ 3 , 4 ] ] , bar : { baz : 6 } } ] ) ).to.be.ok()
-    expect( is.not.deepEqual( createObject( { foo : 1 } ) , createObject( { foo : 2 } ) ) ).to.not.be.ok()
-
   } )
 
 } )
